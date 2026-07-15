af1e030cf5cc832929e475d450124e05e82f52a6
pnnjoroge <paulettenjoroge@gmail.com>
2026-07-15 08:36:05 -0400

    Ch01. Hello World Java Intro

---
+++ Ch01.HelloWorldJavaIntro/src/NjorogeHelloWorldw/NjorogeHelloWorldc.java
@@ -0,0 +1,18 @@
+
+/*
+ * AUTHOR: PAULETTE NJOROGE
+ * DATE: 03/25/2026
+ * PURPOSE: SIMPLE INTRODUCTORY HELLO WORLD PROGRAM
+ */
+
+
+
+package NjorogeHelloWorldw;
+
+public class NjorogeHelloWorldc {
+	public static void main(String[] args)
+	   {
+	      System.out.println("Hello World!");
+	   }
+
+}
---
+++ Ch01.HelloWorldJavaIntro/src/NjorogePersonw/NjorogePersonc.java
@@ -0,0 +1,19 @@
+
+/*
+ * AUTHOR: PAULETTE NJOROGE
+ * DATE: 03/25/2026
+ * PURPOSE: INTRODUCTORY PROGRAM THAT DISPLAYS STUDENT INFORMATION
+ */
+
+package NjorogePersonw;
+
+public class NjorogePersonc {
+	public static void main(String[] args)
+	   {
+	      System.out.println("My name is Paulette Njoroge. \n"
+	    		  			+ "I live in Cary, NC. \n"
+	    		  			+ "My phone number is 123-456-7890 \n"
+	    		  			+ "My major is Web Development.\n"
+	    		  			+ "My dream profession is to be Full Stack Developer.");
+	   }
+}
